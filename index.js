f64999e4a8263097c27325126a5b550e49a51626
supersonic <contact@webretro.ru>
2020-02-18 15:20:47 +0300

    the second task

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 "use strict";
 
 let money = +prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату", "2020-01-01");
-
-let budjetMonth1 = prompt("Введите"  + 
-    "обязательную статью расходов в этом месяце", "");
-let summ1 = +prompt("Во сколько обойдется?", "");
-let budjetMonth2 = prompt("Введите" + 
-    "обязательную статью расходов в этом месяце", "");
-let summ2 = +prompt("Во сколько обойдется?", "");
-
+let time = prompt("Введите дату", "");
 let appData = {
     budjet: money,
     timeData: time,
-    expenses: {
-        budjetMonth1: summ1,
-        budjetMonth2: summ2
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-let budjetDay = (money - summ1 - summ2) / 30;
-console.log("Бюджет на " + time + " составляет: " + budjetDay);
+for (let i=0; i < 2; i++) { // Вариант с урока
+    let budjetMonth = prompt("Введите"  + 
+    "обязательную статью расходов в этом месяце", "");
+    let summ = prompt("Во сколько обойдется?", "");
+    if((typeof(budjetMonth)) === 'string' && (typeof(budjetMonth)) != null 
+    && (typeof(summ)) != null && budjetMonth != '' && summ != '' 
+    && budjetMonth.length < 50) {
+        console.log("Done!");
+        appData.expenses[budjetMonth] = summ;
+    } else {}
+}
 
 /*
-Вопросы к заданию 1
-
-1.Сколько типов данных существует в JS?
-Ответ: 7.
-
-2.Как вывести информацию в консоль?
-Ответ: console.log().
-
-3.Какая функция операторов || и &&?
-Ответ: Операторы ИЛИ и И соответственно.
-
+let i = 0; // Вариант с циклом do while
+do {
+    let budjetMonth = prompt("Введите"  + 
+    "обязательную статью расходов в этом месяце", "");
+    let summ = prompt("Во сколько обойдется?", "");
+    if((typeof(budjetMonth)) === 'string' && (typeof(budjetMonth)) != null 
+    && (typeof(summ)) != null && budjetMonth != '' && summ != '' 
+    && budjetMonth.length < 50) {
+        console.log("Done!");
+        appData.expenses[budjetMonth] = summ;
+    i++;
+    } else {}
+} while (i < 2)
 */
+
+/*
+let i = 0; // Вариант с циклом while
+while (i < 2) {
+    let budjetMonth = prompt("Введите"  + 
+    "обязательную статью расходов в этом месяце", "");
+    let summ = prompt("Во сколько обойдется?", "");
+    if((typeof(budjetMonth)) === 'string' && (typeof(budjetMonth)) != null 
+    && (typeof(summ)) != null && budjetMonth != '' && summ != '' 
+    && budjetMonth.length < 50) {
+        console.log("Done!");
+        appData.expenses[budjetMonth] = summ;
+    i++;
+    } else {}
+}
+*/
+
+appData.budjetDay = appData.budjet / 30;
+console.log("Бюджет на " + time + " составляет: " + appData.budjetDay);
+
+if (appData.budjetDay <= 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.budjetDay > 100 && appData.budjetDay <= 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.budjetDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
