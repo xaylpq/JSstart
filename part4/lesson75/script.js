81dd0dff4b25165b7a2e115c0725ffd8ace417c1
xaylpq <xaylpq@gmail.com>
2020-07-22 21:37:59 +0300

    add les76

---
+++ part4/lesson75/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+try {
+    console.log('Normal');
+    console.log(a);
+    console.log('result');
+} catch(e) {
+    console.log('error');
+} finally {
+    
+}
+
+console.log('Still normal');
